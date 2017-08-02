ec601e7acaed581fc4df37d83c0f6ad2d598c41e
Andre Betz <git@andrebetz.de>
2017-08-02 15:40:45 +0200

    some changes Debugging

--- Software/src/WifiTest/WifiTest.ino
+++ Software/src/WifiTest/WifiTest.ino
@@ -3,103 +3,120 @@
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
 
-#define SSID_ADDR	0
-#define SSID_SIZE	32
-#define PASS_ADDR	SSID_ADDR + SSID_SIZE
-#deinfe PASS_SIZE	64
-
+#define DEBUG_OUT
 ESP8266WebServer server(80);
 
 const char* APssid = "mikroSikaru.de";
 boolean _debug = true;
 
+namespace Debug {
+  template<typename TArg>
+  void debug_out(TArg msg){
+#ifdef DEBUG_OUT
+    Serial.print("M:");
+    Serial.println(msg);
+#endif
+  }
+}
+
 void setUpWifi() {
+
   int n = WiFi.scanNetworks();
 
   String webpage = "";
   webpage =  "<html><head><title>Wifi Setup</title></head>";
-  webpage += "<body><h1><br>Access points</h1>";
+  webpage += "<body>";
+  webpage += "<h1><br>Access points</h1>";
   for (int i = 0; i < n; ++i)
   {
-    webpage += WiFi.SSID(i) + " (";
-    webpage += WiFi.RSSI(i) + ")";
+    webpage += WiFi.SSID(i);
+    webpage += " (";
+    webpage += WiFi.RSSI(i);
+    webpage += ")";
     webpage += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
     webpage += "<BR>";
   }
-  webpage += "<form action='http://" + WiFi.localIP().toString() + "' method='POST'>";
-  webpage += "SSID<input type='text' name='ssis_input'><BR>";
-  webpage += "Password:<input type='text' name='password_input'>&nbsp;<input type='submit' value='Set'>";
-  webpage += "</form></body></html>";
+  webpage += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32>";
+  webpage += "<input name='pass' length=64><input type='submit'></form>";
+  //webpage += "<form action='http://" + WiFi.localIP().toString() + "' method='POST'>";
+  //webpage += "SSID<input type='text' name='ssis_input'><BR>";
+  //webpage += "Password:<input type='text' name='password_input'>&nbsp;<input type='submit' value='Set'>";
+  //webpage += "</form>";
+  webpage += "</body>";
+  webpage += "</html>";
 
   server.send(200, "text/html", webpage);
+  String qsid = server.arg("ssid");
+  String qpass = server.arg("pass");
 
-  if (server.args() > 0 ) { 
-    for ( uint8_t i = 0; i < server.args(); i++ ) {
-      Serial.print(server.argName(i)); // Display the argument
-      Argument_Name = server.argName(i);
-      if (server.argName(i) == "name_input") {
-        Serial.print(" Input received was: ");
-        Serial.println(server.arg(i));
-        Clients_Response1 = server.arg(i);
-      }
-      if (server.argName(i) == "address_input") {
-        Serial.print(" Input received was: ");
-        Serial.println(server.arg(i));
-        Clients_Response2 = server.arg(i);
-      }
+  if (qsid.length() > 0 && qpass.length() > 0) {
+    for (int i = 0; i < 96; ++i) {
+      EEPROM.write(i, 0);
     }
-  }
-
-  boolean testWifi() {
-    String esid = "";
-    String epass = "";
-    for (int i = SSID_ADDR; i < SSID_SIZE; ++i) {
-      esid += char(EEPROM.read(i));
-    }
-    for (int i = PASS_ADDR; i < PASS_SIZE; ++i) {
-      epass += char(EEPROM.read(i));
-    }
-    if ( esid.length() > 1 ) {
-      int c = 0;
-      WiFi.begin(esid.c_str(), epass.c_str());
-      while ( c < 20 ) {
-        delay(500);
-        if (WiFi.status() == WL_CONNECTED)
-          return true;
-        c++;
-      }
-    }
-    return false;
-  }
-
-  void setupAP() {
-    //WiFi.disconnect();
-    IPAddress myIP(10, 1, 1, 10);
-    IPAddress subnet(255, 255, 255, 0);
-    WiFi.mode(WIFI_AP_STA);
-    WiFi.config(myIP, myIP, subnet);
-    WiFi.softAP(APssid);
-    delay(2000);
-  }
-
-  void setup() {
-
-    delay(1000);
-
-    Serial.begin(115200);
-
-    if ( !testWifi() )
+    for (int i = 0; i < qsid.length(); ++i)
     {
-      setupAP();
-      server.on("/", setUpWifi);
+      EEPROM.write(i, qsid[i]);
     }
+    for (int i = 0; i < qpass.length(); ++i)
+    {
+      EEPROM.write(32 + i, qpass[i]);
+    }
+    EEPROM.commit();
+    webpage = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+  }
+}
 
-    server.begin();
+boolean testWifi() {
+  String esid = "";
+  String epass = "";
+  for (int i = 0; i < 32; ++i) {
+    esid += char(EEPROM.read(i));
+  }
+  for (int i = 32; i < 96; ++i) {
+    epass += char(EEPROM.read(i));
+  }
+  if ( esid.length() > 1 ) {
+    int c = 0;
+    WiFi.begin(esid.c_str(), epass.c_str());
+    while ( c < 20 ) {
+      delay(500);
+      if (WiFi.status() == WL_CONNECTED)
+        return true;
+      c++;
+    }
+  }
+  return false;
+}
 
+void setupAP() {
+  //WiFi.disconnect();
+
+  IPAddress myIP(10, 0, 0, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.config(myIP, myIP, subnet);
+  WiFi.softAP(APssid);
+  delay(2000);
+}
+
+void setup() {
+  EEPROM.begin(512);
+  delay(1000);
+
+  Serial.begin(115200);
+
+  if ( !testWifi() )
+  {
+    setupAP();
+    server.on("/", setUpWifi);
   }
 
-  void loop() {
+  server.begin();
 
-    server.handleClient();
+}
 
-  }
+void loop() {
+
+  server.handleClient();
+
+}
